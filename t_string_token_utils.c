47a60a63a822966318505605da2ac30c6490cd49
bpoumeau <delafforest.brieuc@gmail.com>
2023-03-10 00:34:05 +0100

    c'est maginfique

--- t_string_token_utils.c
+++ t_string_token_utils.c
@@ -1,3 +1,37 @@
-//
-// Created by bpoumeau on 3/10/23.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_string_token_utils.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bpoumeau <bpoumeau@student.42lyon.f>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/10 00:25:09 by bpoumeau          #+#    #+#             */
+/*   Updated: 2023/03/10 00:33:40 by bpoumeau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mini_parsing.h"
+
+t_string_token	*string_token_creator(void)
+{
+	t_string_token	*new;
+
+	new = malloc(sizeof(t_string_token));
+	if (new == NULL)
+		return (NULL);
+	new->next = NULL;
+	new->content = NULL;
+	new->token = NONE;
+	return (new);
+}
+
+t_ert	string_token_creator_on(t_string_token **tok_pt)
+{
+	t_string_token *new;
+
+	new = string_token_creator();
+	if (new == NULL)
+		return (MLC_ERR);
+	*tok_pt = new;
+	return (SUCCESS);
+}
